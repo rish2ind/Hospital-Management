2f9594ad46e68fb559bd007295ca066ace8a472c
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-03-09 00:30:23 +0530

    This is the login page

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>Hostpital</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+       <div id="form">
+        <form action="login.php" method="POST">
+           <h2>Login Page</h2>
+            <p>
+                <lable>Email</lable>
+                <input type="email" placeholder="Your Id.." name="email" required="">
+            </p>
+            <p>
+                <lable>Password</lable>
+                <input type="password" placeholder="Password" required="" name="password"><br>
+                <input type="submit" value="Submit" name="submit">
+            </p>
+        </form>
+        </div>
+    </body>
+</html>
