b39c15ed0247045d590c96d5f788b4db71d7548f
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-03-09 00:27:35 +0530

    This is the Home page of Website

---
+++ Main.php
@@ -0,0 +1,43 @@
+   
+   <html>
+    <head>
+        <title>Dashboard</title>
+        <link rel="stylesheet" href="PHPStyle.css">
+    </head>
+    <body>
+        
+        <div id="first">
+            Welcome 
+        </div>
+        <div id="title">
+            HOSPITAL MANAGEMENT SYSTEM
+        </div>
+        <div id="nav">
+           <a href="Main.php">Home</a>  &nbsp; &nbsp; &nbsp;
+            <a href="doctors.php">Doctors</a> &nbsp; &nbsp; &nbsp; &nbsp;
+            <a href="#">Patients</a> &nbsp; &nbsp; &nbsp; &nbsp;
+        </div>
+        <div class="heads">
+            Appointment Lists
+        </div>
+        <table width="600" border="1" id="main_table">
+            <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Gender</th>
+                <th>Age</th>
+            </tr>
+        </table>
+        <div class="heads">
+            Delete Record
+        </div>
+        <table width="600" border="1">
+            <tr>
+                <td>Id</td>
+                <td>Name</td>
+                <td>Gender</td>
+                <td>Age</td>
+            </tr>
+        </table>
+    </body>
+</html>
