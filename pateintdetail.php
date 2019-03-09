e4f639f7102d574aad6634d718469a7638c96e08
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-03-09 23:18:12 +0530

    PHP code of adding details of pateints

---
+++ pateintdetail.php
@@ -0,0 +1,25 @@
+<?php
+        $conn = mysqli_connect("localhost", "root", "", "hospital");
+
+        if(isset($_POST['submit'])){
+            $name = $_POST['patname'];
+            $age = $_POST['patage'];
+            $sex = $_POST['patsex'];
+            $email = $_POST['patemail'];
+            $mobile = $_POST['patmobile'];
+            $month = $_POST['month'];
+            $day = $_POST['day'];
+            $year = $_POST['year'];
+            $dob=$year."-".$month."-".$day;
+            
+            $sql = "insert into pateints (Name, Age, Sex, Email, Mobile, Date) values ('$name', '$age','$sex', '$email', '$mobile', '$dob')";
+            $run = mysqli_query($conn, $sql);
+            if($run){
+                header('Location:Patients.php');
+            }
+            else{
+                echo "Error".mysqli_error($conn);
+            }
+        }
+
+?>
