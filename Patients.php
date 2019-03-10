6aa5896170f58b5a5de44b6d8183159d4d2afa78
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-03-10 15:51:17 +0530

    Now you can search Pateints details

--- Patients.php
+++ Patients.php
@@ -1,5 +1,5 @@
  <?php
- $conn = mysqli_connect("localhost", "root", "", "hospital");
+ $conn = mysqli_connect("localhost", "root", "", "hospital");    
 
 ?>
    <html>
@@ -18,7 +18,7 @@
         <div id="nav">
            <a href="Main.php">Home</a>  &nbsp; &nbsp; &nbsp;
             <a href="doctors.php">Doctors</a> &nbsp; &nbsp; &nbsp; &nbsp;
-            <a href="#">Patients</a> &nbsp; &nbsp; &nbsp; &nbsp;
+            <a href="Patients.php">Patients</a> &nbsp; &nbsp; &nbsp; &nbsp;
         </div>
         <div class="heads">
             Pateints Lists
@@ -53,6 +53,51 @@
            </tr> 
            
            <?php
+            }
+?>     
+        </table>  
+        
+        <div class="heads">
+            Search Pateints
+        </div>
+        
+        <form action="" method="POST">
+            <p>
+                <lable>Enter Mobile</lable>
+                <input type="text" name="mobile" placeholder="Enter Mobile Number..." required="">
+            </p>
+            <input type="submit" name="submit" value="Submit">
+        </form>
+        <table width="100%" border="1" cellpadding="1" cellspacing= "1" id="doctable">
+            <tr>
+                <th>Id</th>
+                <th>Pateints Name</th>
+                <th>Age</th>
+                <th>Sex</th>
+                <th>Email</th>
+                <th>Mobile No.</th>
+                <th>Date</th>
+                <th>Modify</th>    
+            </tr>
+            <?php
+            if(isset($_POST['submit'])){
+            $mobile = $_POST['mobile'];
+    $sqld = "select * from pateints where Mobile = '$mobile'";
+    $result = mysqli_query($conn, $sqld);
+            while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+       ?>
+           <tr>
+               <td><?php echo $row['Id'];?></td>
+               <td><?php echo $row['Name'];?></td>
+               <td><?php echo $row['Age'];?></td>
+               <td><?php echo $row['Sex'];?></td>
+               <td><?php echo $row['Email'];?></td>
+               <td><?php echo $row['Mobile'];?></td>
+               <td><?php echo $row['Date'];?></td>         <td><a href="modifypateints.php?id=<?php echo $row['Email']; ?>">Modify</a></td>    
+           </tr> 
+           
+           <?php
+                }
             }
 ?>     
         </table>  
