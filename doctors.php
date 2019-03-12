1c6d4a2ad7ee5ef0fac453984e5132a5d8d80061
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-03-12 14:04:29 +0530

    Adding logout option

--- doctors.php
+++ doctors.php
@@ -1,16 +1,27 @@
    <?php
     $conn = mysqli_connect("localhost", "root", "", "hospital");
-
+    session_start();
+$userprofile = $_SESSION["user"];
+$sql = "select * from login_details where Username = '".$userprofile."'";
+    $run = mysqli_query($conn, $sql);
+$result = mysqli_fetch_assoc($run);
+if($userprofile==true){
+    
+}
+else{
+    header('location: index.php');
+}
 ?>
    <html>
     <head>
         <title>Dashboard</title>
         <link rel="stylesheet" href="PHPStyle.css">
+        <link rel="icon" href="Images/Hospital.png">
     </head>
     <body>
         
         <div id="first">
-            Welcome
+            Welcome : <?php echo $result['Name']; ?> <a href="logout.php"><span style="float: right;">Logout</span></a>
         </div>
         <div id="title">
           HOSPITAL MANAGEMENT SYSTEM
@@ -48,7 +59,7 @@
                <td><?php echo $row['Age'];?></td>
                <td><?php echo $row['Email'];?></td>
                <td><?php echo $row['Mobile'];?></td>
-               <td><a href="modify.php?id=<?php echo $row['Email']; ?>">Modify</a></td>
+               <td><a href="modify.php?id=<?php echo $row['Id']; ?>">Modify</a></td>
                <td><a href="deletedoctors.php?id=<?php echo $row['Email']; ?>">Delete</a></td>
            </tr> 
            
@@ -59,14 +70,18 @@
         
            <div class="heads">
             Doctors Lists
-        </div>   
+        </div>
+    
+        <div id="docs">   
+         
         <form action="" method="POST">
             <p>
                 <lable>Enter Mobile</lable>
                 <input type="text" name="mobile" placeholder="Enter Mobile Number..." required="">
             </p>
-            <input type="submit" name="submit" value="Submit">
+            <input type="submit" name="submit" value="Submit" >
         </form>
+        </div>
         <table width="100%" border="1" cellpadding="1" cellspacing= "1" id="doctable">
             <tr>
                 <th>Id</th>
@@ -93,7 +108,7 @@
                <td><?php echo $row['Age'];?></td>
                <td><?php echo $row['Email'];?></td>
                <td><?php echo $row['Mobile'];?></td>
-               <td><a href="modify.php?id=<?php echo $row['Email']; ?>">Modify</a></td>
+               <td><a href="modify.php?id=<?php echo $row['Id']; ?>">Modify</a></td>
                <td><a href="deletedoctors.php?id=<?php echo $row['Email']; ?>">Delete</a></td>
            </tr> 
            
@@ -102,5 +117,6 @@
                 }
 ?>
         </table> 
+        
     </body>
 </html>
