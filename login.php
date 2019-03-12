73f52ca56140356e76a17d8d5315ee5a2ec4c354
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-03-12 14:01:16 +0530

    Adding logout coding

--- login.php
+++ login.php
@@ -1,33 +1,22 @@
 <?php
+    session_start();
  $conn = mysqli_connect("localhost", "root", "", "hospital");
-    
-?>
-
-
-<html>
-    <body>
-        <?php
-            if(isset($_POST['submit'])){
+ if(isset($_POST['submit'])){
                 $email = $_POST['email'];
                 $pass = $_POST['password'];
                 $user = $_POST['user'];
-                
+                            
                 $sql = "select * from login_details where Username = '".$user."' AND Email = '".$email."' AND Password = '".$pass."'";
                 $run = mysqli_query($conn, $sql);
+                
                 if(mysqli_num_rows($run)==1){
-                    header('Location: Main.php');
-                   
+                     $_SESSION["user"] = $user;
+                    header('Location: Main.php');              
                 }
                 else{
-                    echo "Wrong email or Password";
-                    ?>
-                    <br> <a href="index.php">Click here to go back</a>
-                    <?php
+                    echo "<script> alert ('Wrong email or Password') </script>";
+                    echo "<script>location.href='index.php'</script>";
                     exit();
                 }
-            }
-            
-        
-        ?>
-    </body>
-</html>
+ }
+?>
